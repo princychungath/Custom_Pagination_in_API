3d79908e06bd859c9c415c15682c3ff21111926d
princy <princyc1998@gmail.com>
2023-07-07 15:32:10 +0530

    project commit

--- libr_project/libr_app/views.py
+++ libr_project/libr_app/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from .pagination import MyCustomPagination
+
 
 
 class BookCreateList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class=MyCustomPagination
 
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
